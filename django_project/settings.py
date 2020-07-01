60e901ba547f536dd89aeb140432d16d919ead67
Sezer Balta <bltsezer@gmail.com>
2020-07-01 12:32:19 +0300

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
